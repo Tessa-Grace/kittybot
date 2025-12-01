78f71edc5216a1cf360e706784784b474008f412
Tessa Gerasimova <tessagerasimova@gmail.com>
2025-12-01 20:10:10 +0200

    logging

--- kittybot.py
+++ kittybot.py
@@ -1,45 +1,56 @@
-# kittybot/kittybot.py
-import requests
-from telebot import TeleBot, types
+import logging
 import os
+import requests
+
+from dotenv import load_dotenv
+from telebot import TeleBot, types
+
+
+load_dotenv()
 
 secret_token = os.getenv('TOKEN')
 bot = TeleBot(token=secret_token)
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+)
+
 URL = 'https://api.thecatapi.com/v1/images/search'
 
-# Код запроса к thecatapi.com и обработку ответа обернём в функцию:
 def get_new_image():
-    response = requests.get(URL).json()
+    try:
+        response = requests.get(URL)
+    except Exception as error:
+        logging.error(f'Ошибка при запросе к основному API: {error}')
+        new_url = 'https://api.thedogapi.com/v1/images/search'
+        response = requests.get(new_url)
+    response = response.json()
     random_cat = response[0].get('url')
     return random_cat
 
 
 @bot.message_handler(commands=['newcat'])
 def new_cat(message):
-    chat = message.chat
-    bot.send_photo(chat.id, get_new_image())
+    chat_id = message.chat.id
+    bot.send_photo(chat_id, get_new_image())
 
 
 @bot.message_handler(commands=['start'])
 def wake_up(message):
-    chat = message.chat
-    name = chat.first_name
+    chat_id = message.chat.id
+    name = message.from_user.first_name
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.row(  # Первая строка кнопок.
-        types.KeyboardButton('Который час?'),  # Создаём первую кнопку в строке.
-        types.KeyboardButton('Определи мой ip'),  # Создаём вторую кнопку в строке.
-    )
-    keyboard.row(  # Вторая строка кнопок.
-        types.KeyboardButton('/random_digit'),  # Создаём кнопку в строке.
-    )
-    
+    button = types.KeyboardButton('/newcat')
+    keyboard.add(button)
+
     bot.send_message(
-        chat_id=chat.id,
-        text=f'Спасибо, что вы включили меня, {name}!',
-        reply_markup=keyboard,  # Отправляем пользователю текстовый ответ и клавиатуру.
+        chat_id=chat_id,
+        text=f'Привет, {name}. Посмотри, какого котика я тебе нашел',
+        reply_markup=keyboard,
     )
 
-    bot.send_photo(chat.id, get_new_image())
+    bot.send_photo(chat_id, get_new_image())
 
 
 @bot.message_handler(content_types=['text'])
@@ -48,4 +59,10 @@ def say_hi(message):
     chat_id = chat.id
     bot.send_message(chat_id=chat_id, text='Привет, я KittyBot!')
 
-bot.polling() 
+
+def main():
+    bot.polling(none_stop=True)
+
+
+if __name__ == '__main__':
+    main()
